37f813252f33e74e8489d07de666cea108ff8f3e
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-18 21:06:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,10 +5,14 @@ prince: "purpleRain"
  }
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   playlist["Phil Ochs"] = "Here's to the State of Mississippi"
     return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.Kanye;
+=======
+  object[jonB] = "coolRelax"
+>>>>>>> 6d9a388c0a351c583536dcd05ec54d38d1bd827a
   return playlist
 }
